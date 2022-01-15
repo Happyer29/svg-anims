561b5f3ab87ce7818ad3c0ad419466537b672f49
happyer29 <happyer.present@gmail.com>
2022-01-16 02:08:17 +0300

    accepted

--- index.js
+++ index.js
@@ -12,73 +12,163 @@ let deltaHeight__whiteLine = 0;
 
 
 //ACTION 1
-const action1__stepDuration = 200;
+const action1__stepDuration = 300;
 const action1__easing = "easeInOutCirc"
 
 let timeline = anime.timeline({
-    easing: action1__easing,
-    duration: action1__stepDuration,
+    easing: action1__easing
 });
 
-let action1__blueGradient = {
-    targets: '#blue-line',
-    height:  [0, 290],
-}
-
-let action1__redGradient = {
-    targets: '#red-line',
-    height: [0, 460],
-};
-
-let action1__whiteGradient = {
-    targets: '#white-line',
-    height: [0, 680],
-};
-
-
 //ACTION 2
-const action2__stepDuration = 100;
+const action2__stepDuration = 400;
+const action2__easing = "easeInOutCirc";
 
-let action2 = {
-    targets: ['#blue-line', '#red-line', '#white-line'],
-    rotate: [-90, -4],
-    translateY: [-310, -25],
-    translateX: [-130, -130],
-    duration: action2__stepDuration
-}
-let action2__blueGradient = {
-    targets: '#blue-line',
-    height:  [290, startHeight__blueLine],
-    duration: action2__stepDuration
-}
-console.log(startHeight__blueLine);
-let action2__redGradient = {
-    targets: '#red-line',
-    height: [460, startHeight__redLine],
-    duration: action2__stepDuration
-};
-
-let action2__whiteGradient = {
+let whiteGradient = {
     targets: '#white-line',
-    height: [680, startHeight__whiteLine],
-    duration: action2__stepDuration
-};
+    height: [
+        {
+            value: [0, 1100],
+            duration: action1__stepDuration
+        },
+        {
+            value: startHeight__whiteLine,
+            duration: action2__stepDuration,
+        }
+    ],
+    rotate: [
+        {
+            value: -90,
+            duration: 0,
+            delay: action1__stepDuration
+        },
+        {
+            value: -4,
+            duration: action2__stepDuration,
+            easing: action1__easing
+        }
+    ],
+    translateY: [
+        {
+            value: -100,
+            duration: 0,
+            delay: action1__stepDuration
+        },
+        {
+            value: -25,
+            duration: action2__stepDuration,
+        }
+    ],
+    translateX: [
+        {
+            value: -130,
+            duration: 0,
+            delay: action1__stepDuration
+        }
+    ],
 
+}
 
+let redGradient = {
+    targets: '#red-line',
+    height: [
+        {
+            value: [0, 660],
+            duration: action1__stepDuration,
+        },
+        {
+            value: startHeight__redLine,
+            duration: action2__stepDuration,
+            delay: action1__stepDuration
+        }
+    ],
+    rotate: [
+        {
+            value: -90,
+            duration: 0,
+            delay: action1__stepDuration * 2
+        },
+        {
+            value: -4,
+            duration: action2__stepDuration,
+            easing: action1__easing
+        }
+    ],
+    translateY: [
+        {
+            value: -100,
+            duration: 0,
+            delay: action1__stepDuration * 2
+        },
+        {
+            value: -25,
+            duration: action2__stepDuration,
+        }
+    ],
+    translateX: [
+        {
+            value: -130,
+            duration: 0,
+            delay: action1__stepDuration
+        }
+    ],
+
+}
+
+let blueGradient = {
+    targets: '#blue-line',
+    height: [
+        {
+            value: [0, 560],
+            duration: action1__stepDuration,
+        },
+        {
+            value: startHeight__blueLine,
+            duration: action2__stepDuration,
+        }
+    ],
+    rotate: [
+        {
+            value: -90,
+            duration: 0,
+            delay: action1__stepDuration
+        },
+        {
+            value: -4,
+            duration: action2__stepDuration,
+            easing: action1__easing
+        }
+    ],
+    translateY: [
+        {
+            value: -100,
+            duration: 0,
+            delay: action1__stepDuration
+        },
+        {
+            value: -25,
+            duration: action2__stepDuration,
+        }
+    ],
+    translateX: [
+        {
+            value: -130,
+            duration: 0,
+            delay: action1__stepDuration
+        }
+    ],
+
+}
+
+let animeTime = "-=" + (action1__stepDuration + action2__stepDuration);
 timeline
-    //action 1
-    .add(action1__whiteGradient)
-    .add(action1__redGradient)
-    .add(action1__blueGradient)
-    //action 2
-    .add(action2)
-    .add(action2__whiteGradient, "-=100")
-    .add(action2__redGradient, "-=100")
-    .add(action2__blueGradient, "-=100")
+    .add(whiteGradient)
+    .add(redGradient, "-=" + (action2__stepDuration))
+    .add(blueGradient, "-=" + (action1__stepDuration + action2__stepDuration));
+
 
 
 //ACTION 3
-const duration = 3500;
+const duration = 5500;
 const easing = "easeInOutSine"
 
 timeline.finished.then(()=>{
@@ -94,7 +184,7 @@ timeline.finished.then(()=>{
     anime({
         targets: '#red-line',
         height: startHeight__redLine + deltaHeight__redLine,
-        rotate: [-4, -3],
+        rotate: [-4, -3.3],
         duration: duration,
         direction: 'alternate',
         easing: easing,
@@ -110,8 +200,6 @@ timeline.finished.then(()=>{
         loop: true,
     })
 })
-
-
 
 // anime({
 //     targets: ['#blue-line', '#red-line', '#white-line'],
